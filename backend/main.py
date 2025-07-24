109455176f018af3c5affe3fd7b64122145724e0
Ehab Kayyali <KayyaliEhab25@gmail.com>
2025-07-24 19:42:52 -0400

    Update backend to match latest main branch

--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,15 @@
-# main.py
 import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import users, courses, tasks
-from fastapi.middleware.cors import CORSMiddleware
-
-
 
 # --- Application Setup ---
-
 # Initialize FastAPI app
 app = FastAPI(
     title="CourseWork Lite API",
     description="API for managing academic tasks and courses.",
     version="1.0.0"
-)
-
-
-# Add CORS Middleware so the frontend can talk to the backend
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite dev server
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
 )
 
 # Load environment variables from .env file
@@ -46,13 +31,13 @@ if origins:
 else:
     raise Exception("CORS middleware not configured. No CORS_ORIGINS environment variable found.")
 
-
 # Include routers for different functionalities / endpoints
 app.include_router(users.router)
 app.include_router(courses.router)
 app.include_router(tasks.router)
 
-# Root Endpoint
+# --- Root Endpoint ---
+
 @app.get("/", tags=["Root"])
 async def read_root():
     """A simple root endpoint to confirm the API is running."""
